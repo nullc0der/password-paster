5028b53d67151059b000a24d7caf3c0605ba8f7c
Prasanta Kakati <prasantakakati1994@gmail.com>
2023-08-10 15:52:58 +0530

    Improved visibility toggle, fixed some css and wrapped password input in a form

--- README.md
+++ README.md
@@ -4,6 +4,14 @@
 
 Frequently, we encounter websites that restrict pasting within their password input fields, like the login pages of banking sites. This compels us to manually input the password, which can be even more laborious when dealing with lengthy passwords from a password manager. Moreover, certain websites necessitate entering the password twice during sign-up, demanding a repetitive manual entry. This is where this extension becomes useful.
 
+#### Install
+
+For installation you can either download from github release page or from marketplace
+
+- [https://addons.mozilla.org/en-US/firefox/addon/password-paster/](https://addons.mozilla.org/en-US/firefox/addon/password-paster/)
+
+- [https://github.com/nullc0der/password-paster/releases][https://github.com/nullc0der/password-paster/releases]
+
 #### How to use
 
 - Choose the website's input field.
--- browserAction/script.js
+++ browserAction/script.js
@@ -2,6 +2,7 @@ const passwordInputField = document.getElementById("passwordInput");
 const submitBtn = document.getElementById("submitBtn");
 const messageBox = document.getElementById("messageBox");
 const visibilityToggle = document.getElementById("visibilityToggle");
+const passwordForm = document.getElementById("passwordForm");
 
 function showMessage(data) {
   submitBtn.innerText = "paste";
@@ -14,7 +15,8 @@ function showMessage(data) {
   }, 5000);
 }
 
-submitBtn.addEventListener("click", () => {
+passwordForm.addEventListener("submit", (e) => {
+  e.preventDefault();
   function sendPassword(tabs) {
     browser.tabs.sendMessage(tabs[0].id, {
       command: "pastePassword",
@@ -32,12 +34,12 @@ submitBtn.addEventListener("click", () => {
 });
 
 visibilityToggle.addEventListener("click", () => {
-  visibilityToggle.innerText = "visibility_off";
-  if (passwordInputField.type === "password") {
-    passwordInputField.type = "text";
-  } else {
-    passwordInputField.type = "password";
-  }
+  const passwordHidden = passwordInputField.type === "password";
+  Object.assign(visibilityToggle, {
+    title: passwordHidden ? "Hide Password" : "Show Password",
+    innerText: passwordHidden ? "visibility_off" : "visibility",
+  });
+  passwordInputField.type = passwordHidden ? "text" : "password";
 });
 
 browser.runtime.onMessage.addListener((message) => {
